3906219ced74827e755fd4ac3be6675ac51cb185
Kotrel Manurung <kotrel@MacBook-Pro-Kotrel.local>
2025-01-04 13:22:39 +0700

    fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
